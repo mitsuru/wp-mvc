b5410f7f0eb815c1ba3d78744d58d02f6e611476
Brandon Hansen <brandonh@ibethel.org>
2013-06-12 10:16:54 -0700

    Make dispatcher set controller to static var to be accessed elsewhere

--- core/mvc_dispatcher.php
+++ core/mvc_dispatcher.php
@@ -2,6 +2,8 @@
 
 class MvcDispatcher {
 
+	public static $controller;
+
 	function dispatch($options=array()) {
 		
 		$controller_name = $options['controller'];
@@ -10,13 +12,13 @@ class MvcDispatcher {
 		
 		$controller_class = MvcInflector::camelize($controller_name).'Controller';
 		
-		$controller = new $controller_class();
+		MvcDispatcher::$controller = new $controller_class();
 		
-		$controller->name = $controller_name;
-		$controller->action = $action;
-		$controller->init();
+		MvcDispatcher::$controller->name = $controller_name;
+		MvcDispatcher::$controller->action = $action;
+		MvcDispatcher::$controller->init();
 		
-		if (!method_exists($controller, $action)) {
+		if (!method_exists(MvcDispatcher::$controller, $action)) {
 			MvcError::fatal('A method for the action "'.$action.'" doesn\'t exist in "'.$controller_class.'"');
 		}
 		
@@ -29,23 +31,23 @@ class MvcDispatcher {
 			unset($params['page']);
 		}
 		
-		$controller->params = $params;
-		$controller->set('this', $controller);
-		if (!empty($controller->before)) {
-			foreach ($controller->before as $method) {
-				$controller->{$method}();
+		MvcDispatcher::$controller->params = $params;
+		MvcDispatcher::$controller->set('this', MvcDispatcher::$controller);
+		if (!empty(MvcDispatcher::$controller->before)) {
+			foreach (MvcDispatcher::$controller->before as $method) {
+				MvcDispatcher::$controller->{$method}();
 			}
 		}
-		$controller->{$action}();
-		if (!empty($controller->after)) {
-			foreach ($controller->after as $method) {
-				$controller->{$method}();
+		MvcDispatcher::$controller->{$action}();
+		if (!empty(MvcDispatcher::$controller->after)) {
+			foreach (MvcDispatcher::$controller->after as $method) {
+				MvcDispatcher::$controller->{$method}();
 			}
 		}
-		$controller->after_action($action);
+		MvcDispatcher::$controller->after_action($action);
 		
-		if (!$controller->view_rendered) {
-			$controller->render_view($controller->views_path.$action, $options);
+		if (!MvcDispatcher::$controller->view_rendered) {
+			MvcDispatcher::$controller->render_view($controller->views_path.$action, $options);
 		}
 	
 	}
