b2e00b9f57db03e11ab55bf86ee85fae00859799
Andrew Scofied <apotropaics@gmail.com>
2013-06-23 21:18:44 -0700

    declaring static functions where they were being called

--- core/mvc_model_registry.php
+++ core/mvc_model_registry.php
@@ -4,7 +4,7 @@ class MvcModelRegistry {
 
 	var $__models = array();
 
-	private function &get_instance() {
+	private static function &get_instance() {
 		static $instance = array();
 		if (!$instance) {
 			$instance[0] = new MvcModelRegistry();
@@ -12,27 +12,27 @@ class MvcModelRegistry {
 		return $instance[0];
 	}
 
-	public function &get_model($key) {
-		$_this =& self::get_instance();
+	public static function &get_model($key) {
+		$_this = self::get_instance();
 		$key = MvcInflector::camelize($key);
 		$return = false;
 		if (isset($_this->__models[$key])) {
-			$return =& $_this->__models[$key];
+			$return = $_this->__models[$key];
 		} else if (class_exists($key)) {
 			$_this->__models[$key] = new $key();
-			$return =& $_this->__models[$key];
+			$return = $_this->__models[$key];
 		}
 		return $return;
 	}
 
-	public function &get_models() {
-		$_this =& self::get_instance();
-		$return =& $_this->__models;
+	public static function &get_models() {
+		$_this = self::get_instance();
+		$return = $_this->__models;
 		return $return;
 	}
 	
-	public function add_model($key, &$model) {
-		$_this =& self::get_instance();
+	public static function add_model($key, &$model) {
+		$_this = self::get_instance();
 		$key = MvcInflector::camelize($key);
 		if (!isset($_this->__models[$key])) {
 			$_this->__models[$key] = $model;
--- core/mvc_router.php
+++ core/mvc_router.php
@@ -4,7 +4,7 @@ class MvcRouter {
 	
 	public $routes = array();
 
-	public function public_url($options=array()) {
+	public static function public_url($options=array()) {
 		$options = apply_filters('mvc_before_public_url', $options);
 		$defaults = array(
 			'action' => 'index',
@@ -82,7 +82,7 @@ class MvcRouter {
 		return $url;
 	}
 
-	public function admin_url($options=array()) {
+	public static function admin_url($options=array()) {
 		if (!empty($options['object']) && is_object($options['object'])) {
 			if (empty($options['id']) && !empty($options['object']->__id)) {
 				$options['id'] = $options['object']->__id;
@@ -99,7 +99,7 @@ class MvcRouter {
 		return $url;
 	}
 	
-	public function admin_url_params($options=array()) {
+	public static function admin_url_params($options=array()) {
 		$params = array();
 		if (!empty($options['controller'])) {
 			$controller = preg_replace('/^admin_/', '', $options['controller']);
@@ -114,7 +114,7 @@ class MvcRouter {
 		return $params;
 	}
 	
-	public function admin_page_param($options=array()) {
+	public static function admin_page_param($options=array()) {
 		if (is_string($options)) {
 			$options = array('model' => $options);
 		}
@@ -124,17 +124,17 @@ class MvcRouter {
 		return false;
 	}
 
-	public function public_connect($route, $defaults=array()) {
-		$_this =& MvcRouter::get_instance();
+	public static function public_connect($route, $defaults=array()) {
+		$_this = MvcRouter::get_instance();
 		$_this->add_public_route($route, $defaults);
 	}
 	
-	public function admin_ajax_connect($route) {
-		$_this =& MvcRouter::get_instance();
+	public static function admin_ajax_connect($route) {
+		$_this = MvcRouter::get_instance();
 		$_this->add_admin_ajax_route($route);
 	}
 
-	private function &get_instance() {
+	private static function &get_instance() {
 		static $instance = array();
 		if (!$instance) {
 			$instance[0] = new MvcRouter();
@@ -146,25 +146,25 @@ class MvcRouter {
 		return $instance[0];
 	}
 
-	public function &get_public_routes() {
-		$_this =& self::get_instance();
-		$return =& $_this->routes['public'];
+	public static function &get_public_routes() {
+		$_this = self::get_instance();
+		$return = $_this->routes['public'];
 		return $return;
 	}
 
-	public function &get_admin_ajax_routes() {
-		$_this =& self::get_instance();
-		$return =& $_this->routes['admin_ajax'];
+	public static function &get_admin_ajax_routes() {
+		$_this = self::get_instance();
+		$return = $_this->routes['admin_ajax'];
 		return $return;
 	}
 	
-	public function add_public_route($route, $defaults) {
-		$_this =& self::get_instance();
+	public static function add_public_route($route, $defaults) {
+		$_this = self::get_instance();
 		$_this->routes['public'][] = array($route, $defaults);
 	}
 	
-	public function add_admin_ajax_route($route) {
-		$_this =& self::get_instance();
+	public static function add_admin_ajax_route($route) {
+		$_this = self::get_instance();
 		if (empty($route['wp_action'])) {
 			$route['wp_action'] = $route['controller'].'_'.$route['action'];
 		}
