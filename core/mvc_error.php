11fd2ac078cf69d0989126a2a01dd78ed3375b27
Andrew Scofied <apotropaics@gmail.com>
2013-09-13 14:37:42 -0700

    added debug_tracer function to MvcError

--- core/mvc_error.php
+++ core/mvc_error.php
@@ -14,6 +14,24 @@ class MvcError {
 	public static function notice($message) {
 		self::write('notice', $message);
 	}
+
+	public static function debug_tracer(){
+		$trace = debug_backtrace(); 
+
+		$caller = array_shift($trace); 
+		$function_name = $caller['function']; 
+		error_log(sprintf('%s: Called from %s:%s', $function_name, $caller['file'], $caller['line'])); 
+		foreach ($trace as $entry_id => $entry) { 
+				$entry['file'] = $entry['file'] ? : '-'; 
+				$entry['line'] = $entry['line'] ? : '-'; 
+				if (empty($entry['class'])) { 
+						error_log(sprintf('%s %3s. %s() %s:%s', $function_name, $entry_id + 1, $entry['function'], $entry['file'], $entry['line'])); 
+				} else { 
+						error_log(sprintf('%s %3s. %s->%s() %s:%s', $function_name, $entry_id + 1, $entry['class'], $entry['function'], $entry['file'], $entry['line'])); 
+				} 
+		} 
+
+	}
 	
 	private static function write($type_key, $message) {
 	
